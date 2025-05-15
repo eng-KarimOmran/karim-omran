5eaa871a8d51cc79868081953a907ff7f1c792be
Karim Omran <eng.karimomran@gmail.com>
2025-05-15 23:13:36 +0300

    HomeCount

---
+++ app/_comonents/Home/StatsHome/Count.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+
+export default function Count({ num }) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prev => {
+        if (prev < num) {
+          return prev + 1;
+        } else {
+          clearInterval(interval);
+          return prev;
+        }
+      });
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, [num]);
+
+  return (
+    <div className='text-5xl text-white'>
+      {count}
+    </div>
+  );
+}
--- app/_comonents/Home/StatsHome/StatsHome.jsx
+++ app/_comonents/Home/StatsHome/StatsHome.jsx
@@ -1,14 +1,15 @@
-"use client"
 import React from "react";
-import CountUp from "react-countup";
+import Count from "./Count";
 
 export default function StatsHome() {
+
   const stats = [
     { content: "Years Experience", num: 1 },
     { content: "Projects Delivered", num: 6 },
     { content: "Technologies Mastered", num: 7 },
-    { content: "Code Contributions", num: 30 },
+    { content: "Code Contributions", num: 10 },
   ];
+
   return (
     <div className="flex items-center justify-center md:justify-between w-full my-2 flex-wrap">
       {stats.map((stat) => (
@@ -16,15 +17,7 @@ export default function StatsHome() {
           key={stat.content}
           className="flex items-center gap-1 justify-center w-full md:w-6/12 lg:w-auto max-w-[250px] pb-3 pt-8 border-b-[1px] border-gray-300/20"
         >
-          <span className="text-5xl text-white">
-            <CountUp
-              prefix={stat.content === "Years Experience" ? "+" : ""}
-              delay={1}
-              start={0}
-              end={stat.num}
-              duration={5}
-            />
-          </span>
+          <Count num={stat.num} />
           <span className="max-w-[100px]">{stat.content}</span>
         </div>
       ))}
